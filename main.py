5a11f93948557d01bd007cbe87eb899719256ee4
Dhairya Patel <p5d7@proton.me>
2023-03-30 19:11:56 +0530

    Update main.py
    >> Done
    > Updated first question based on old file
    >> Left-To-Do
    > Make ML algorithm run. Issue still persisting

--- main.py
+++ main.py
@@ -10,18 +10,35 @@ Please look at script.py file in scrapper directory if you want to change how to
 
 player_data = pd.read_csv("player-data.csv")
 
-# Information-1
-# Is player positions structure same across all teams?
-print("Information-1")
-print("How does playing positions of player look like across all teams? Is it same for all teams?")
-playing_positions = player_data.groupby(["Team", "Position"]).size().unstack()
-ax = playing_positions.plot(kind="bar", stacked = True, color=["red", "blue", "green", "yellow", "cyan"],
-                            edgecolor="black")
-ax.set_xlabel("Team")
-ax.set_ylabel("Number of players")
-ax.set_title("Player positions across all teams")
-plt.show()
-print("Conclusion for Question-1")
-print("No. It looks like there is no common structure for positions. No two teams have same structure for team."
-      "The reason could be each team have different star player in different position")
 
+def research_question1(pd_data):
+    print("Question - 1")
+    print("""Is player positions structure same across all teams? What I like to know is if all teams favour same ratio 
+    for different roles such as batsman, bowler, wicketkeeper and so on.""")
+    print("Answer - 1")
+    print("First, let us look at frequency table of player roles and team")
+
+    frequency_table = pd_data.pivot_table(values='Name', index=['Team'], columns=['Position'], aggfunc=len,
+                                          fill_value=0, margins=True)
+
+    print("Frequency Table")
+    print(frequency_table)
+
+    print("From the frequency table, we can clearly see that not all teams are following same ratio between their roles"
+          )
+
+    print("Now, let us look at stacked bar chart for better understanding")
+    playing_positions = player_data.groupby(["Team", "Position"]).size().unstack()
+    ax = playing_positions.plot(kind="bar", stacked=True, color=["red", "blue", "green", "yellow", "cyan"],
+                                edgecolor="black")
+    ax.set_xlabel("Team")
+    ax.set_ylabel("Number of players")
+    ax.set_title("Player positions across all teams")
+    plt.show()
+    print("Conclusion for Question-1")
+    print("No. It looks like there is no common structure for positions. No two teams have same structure for team."
+          "The reason could be each team have different star player in different position, if one team has 2 star "
+          "bowlers, they are less likely to add more back up bowlers to their team")
+
+
+# research_question1(player_data)
