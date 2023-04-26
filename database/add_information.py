4d5210e1807a41e36309d54c002f5a7b1072e4b7
Dhairya Patel <p5d7@proton.me>
2023-04-26 22:54:31 +0530

    Added partial information of match-1
    >> Summary
    > Partial information of match-1 added
    
    >> Thoughts
    > Should I seperate bowling and fielding tables for better data management?

--- database/add_information.py
+++ database/add_information.py
@@ -1,24 +1,39 @@
-
-from database_table_add import update_player_stat
-
-"""
-match_id, opponent_team, player_name, batting_run=0, batting_bowl=0, batting_4=0, batting_6=0, out_style=None,
-not_out=0, catch=0, run_out=0, stumping=0, 
-bowling_bowl=0, bowling_run=0, wicket_taken=0, wicket_taken_catch=0, wicket_taken_bowled=0, wicket_taken_stump=0
-"""
+from database_table_add import add_match, add_player_bat_data, add_player_bowl_data
 
 
 # Match-1
-# def match1():
-#     add_match(1, "GT", "CSK", "GT", 1, "GT", "Rashid Khan", 1, "178/7", "182/5", 92, "Ruturaj Gaikwad", "Rashid Khan",
-#               "26/2", 63, "Shubman Gill", "Rajvardhan Hangargekar", "36/3")
-#     update_player_stat("Devon Conway", 1, "GT", 1, 6, 0, 0, "bowled/Mohammed Shami", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-#     update_player_stat("Ruturaj Gaikwad", 1, "GT", 92, 50, 4, 9, "catch/Alzarri Joseph", 0, 1, 0, 0, 0, 0, 0, 0, 0, 0)
-#     update_player_stat("Moeen Ali", 1, "GT", 23, 17, 4, 1, "catch/Rashid Khan", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-#     update_player_stat("Ben Stokes", 1, "GT", 7, 6, 1, 0, "catch/Rashid Khan", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-#     update_player_stat("Ambati Rayudu", 1, "GT", 12, 12, 0, 1, "bowled/Joshua Little", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-#     update_player_stat("Shivam Dube ", 1, "GT", 19, 18, 0, 1, "catch/Mohammed Shami", 0, 1, 0, 0, 0, 0, 0, 0, 0, 0)
-#     update_player_stat("Ravindra Jadeja", 1, "GT", 1, 2, 0, 0, "catch/Alzarri Joseph", 0, 0, 0, 0, )
+def match1() -> str:
+    """
+    Match-1 was played between CSK and GT in home ground of GT, Narendra Modi stadium. It was opening match of IPL 2023.
+    GT won this match in thrilling way.
+    """
+    # Add match
+    add_match(1, "GT", "CSK", "Narendra Modi Stadium", "GT", True, "GT", "Rashid Khan", True, "178/7", "182/5",
+              "51/2", "65/1", 92, "Ruturaj Gaikwad", "Rashid Khan", "26/2", 63, "Shubman Gill", "Rajvardhan Hangargekar"
+              , "36/3")
+
+    # Add player performance
+    # Inning-1 => CSK BAT and GT Field
+    # CSK BAT
+    add_player_bat_data("Devon Conway", 1, "GT", 1, 6, 0, 0, False, "bowled", "Mohammed Shami", None, False)
+    add_player_bat_data("Ruturaj Gaikwad", 1, " GT", 92, 50, 4, 9, False, "catch", "Alzarri Joseph", "Shubman Gill",
+                        False)
+    add_player_bat_data("Moeen Ali", 1, "GT", 23, 17, 4, 1, False, "catch", "Rashid Khan", "Wriddhiman Saha", False)
+    add_player_bat_data("Ben Stokes", 1, "GT", 7, 6, 1, 0, False, "catch", "Rashid Khan", "Wriddhiman Saha", False)
+    add_player_bat_data("Ambati Rayudu", 1, "GT", 12, 12, 0, 1, False, "bowled", "Joshua Little", False)
+    add_player_bat_data("Shivam Dube", 1, "GT", 19, 18, 0, 1, False, "catch", "Mohammed Shami", "Rashid Khan", False)
+    add_player_bat_data("Ravindra Jadeja", 1, "GT", 1, 2, 0, 0, False, "catch", "Alzarri Joseph", "Vijay Shankar", False
+                        )
+    add_player_bat_data("MS Dhoni", 1, "GT", 14, 7, 1, 1, True, motm=False)
+    add_player_bat_data("Mitchell Santner ", 1, 3, 0, 0, True, motm=False)
+
+    # GT Field
+    add_player_bowl_data("Mohammed Shami", 1, "GT", 24, 29, 2, 0, 13, 2, 2, 0, 1, 1, 1, 0, 0, 0, 0, 0, False)
+    add_player_bowl_data("Hardik Pandya", 1, "GT", 18, 28, 0, 0, 6, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, False)
+
+    # Work in progress 
+
+    return "Data added successfully for match-1"
 
 # match1()
 # Santner, Dhoni, Gaikwad, Dube
--- database/database_table_add.py
+++ database/database_table_add.py
@@ -1,101 +1,101 @@
 import sqlite3
 
 
-def update_player_stat(player_name, batting_run=0, batting_bowl=0, batting_4=0, batting_6=0,
-                       out_style=None, not_out=0, catch=0, run_out=0, stumping=0, bowling_bowl=0, bowling_run=0,
-                       wicket_taken=0, wicket_taken_catch=0, wicket_taken_bowled=0, wicket_taken_stump=0):
-    """
-            Adds data to the player's record for a match they played in
-
-            Parameters:
-            batting_run (int): the number of runs scored by the player while batting
-            batting_bowl (int): the number of balls faced by the player while batting
-            batting_4 (int): the number of fours scored by the player while batting
-            batting_6 (int): the number of sixes scored by the player while batting
-            out_style (str): the way the player was dismissed (catch, run-out, stump, bowled) or None if not out
-            not_out (int): 1 if the player was not out, 0 otherwise
-            catch (int): the number of catches taken by the player in the field
-            run_out (int): the number of run outs executed by the player in the field
-            stumping (int): the number of stumpings executed by the player in the field
-            bowling_bowl (int): the number of balls bowled by the player
-            bowling_run (int): the number of runs given away by the player while bowling
-            wicket_taken (int): the number of wickets taken by the player while bowling
-            wicket_taken_catch (int): the number of wickets taken by the player through catches
-            wicket_taken_bowled (int): the number of wickets taken by the player through bowled
-            wicket_taken_stump (int): the number of wickets taken by the player through stumping
-
-            Returns:
-            None
-    """
-
-    # Create a connection with database
-    ipl_db = sqlite3.connect("ipl.db")
-    ipl_cursor = ipl_db.cursor()
-
-    query = "UPDATE player_stat " \
-            "SET match_played = match_player + 1, batting_runs_scored = batting_runs_scored + ?," \
-            "batting_bowls_played = batting_bowls_played + ?, batting_boundary = batting_boundary + ?," \
-            "batting_six = batting_six + ?, batting_half_century = batting_half_century + ?, " \
-            "batting_century = batting_century + ?, batting_duck_out = batting_duck_out + ?, " \
-            "highest_score = highest_score + ?, batting_not_out = batting_not_out + ?, out_catch = out_catch + ?" \
-            "out_run_out = out_run_out + ?, out_stumped = out_stumped + ?, out_bowled = out_bowled + ?, " \
-            "field_catch = field_catch + ?, field_run_out = field_run_out + ?, field_stumping = field_stumping + ?," \
-            "bowling_bowls = bowling_bowls + ?, bowling_runs = bowling_runs + ?, bowling_wickets = bowling_wickets+?," \
-            "bowling_wicket_catch = bowling_wicket_catch + ?, bowling_wicket_bowled = bowling_wicket_bowled + ?," \
-            "bowling_wicket_stump = bowling_wicket_stump + ?, bowling_best_figure = ? , " \
-            "bowling_five_wickets = bowling_five_wickets + ?" \
-            "WHERE player_name = ? LIMIT 1;"
-
-    # Check if player hit half-century(50 runs) or century(100 runs)
-    century = True if batting_run >= 100 else False
-    half_century = True if 50 <= batting_run < 100 else False
-
-    # Check if player is duck out/got out on 0
-    duck_out = True if batting_run == 0 else False
-
-    # Check if new score of player is higher than previous highest score
-    current_high_score = \
-        ipl_cursor.execute('SELECT highest_score FROM player_stat WHERE name = ?', (player_name,)).fetchone()[0]
-    new_high_score = max(batting_run, current_high_score)
-
-    # Type of how player got out
-    # I am assuming that input provided by user is correct as currently I am the only one providing input
-    out_catch = 1 if out_style == "catch" else 0
-    out_run_out = 1 if out_style == "run-out" else 0
-    out_stumped = 1 if out_style == "stumped" else 0
-    out_bowled = 1 if out_style == "bowled" else 0
-
-    # Check if current bowling figure is best bowling figure of bowler.
-
-    current_best_bowling_figure = ipl_cursor.execute("SELECT bowling_best_figure FROM player_stat WHERE name = ?",
-                                                     (player_name,)).fetchone()[0]
-    if wicket_taken > 0:
-        current_best_bowling_figure = ipl_cursor.execute("SELECT bowling_best_figure FROM player_stat WHERE name = ?",
-                                                         (player_name,)).fetchone()[0]
-        if wicket_taken >= int(current_best_bowling_figure.split("/")[1]):
-            new_best_bowling_figure = f"{min(current_best_bowling_figure.split('/')[0], bowling_run)}/" \
-                                      f"{current_best_bowling_figure.split('/')[1]}"
-        else:
-            new_best_bowling_figure = current_best_bowling_figure
-    else:
-        new_best_bowling_figure = current_best_bowling_figure
-
-    # Check if bowler got more than or equal to 5 wickets
-    five_wickets = 1 if wicket_taken >= 5 else 0
-
-    ipl_cursor.execute(query, (batting_run, batting_bowl, batting_4, batting_6, int(half_century), int(century),
-                               int(duck_out), new_high_score, not_out, out_catch, out_run_out, out_stumped,
-                               out_bowled, catch, run_out, stumping, bowling_bowl, bowling_run, wicket_taken,
-                               wicket_taken_catch, wicket_taken_bowled, wicket_taken_stump, new_best_bowling_figure,
-                               five_wickets, player_name,))
-
-    ipl_db.commit()
-    ipl_cursor.close()
-    ipl_db.close()
-
+# def update_player_stat(player_name, batting_run=0, batting_bowl=0, batting_4=0, batting_6=0,
+#                        out_style=None, not_out=0, catch=0, run_out=0, stumping=0, bowling_bowl=0, bowling_run=0,
+#                        wicket_taken=0, wicket_taken_catch=0, wicket_taken_bowled=0, wicket_taken_stump=0):
+#     """
+#             Adds data to the player's record for a match they played in
+#
+#             Parameters:
+#             batting_run (int): the number of runs scored by the player while batting
+#             batting_bowl (int): the number of balls faced by the player while batting
+#             batting_4 (int): the number of fours scored by the player while batting
+#             batting_6 (int): the number of sixes scored by the player while batting
+#             out_style (str): the way the player was dismissed (catch, run-out, stump, bowled) or None if not out
+#             not_out (int): 1 if the player was not out, 0 otherwise
+#             catch (int): the number of catches taken by the player in the field
+#             run_out (int): the number of run outs executed by the player in the field
+#             stumping (int): the number of stumpings executed by the player in the field
+#             bowling_bowl (int): the number of balls bowled by the player
+#             bowling_run (int): the number of runs given away by the player while bowling
+#             wicket_taken (int): the number of wickets taken by the player while bowling
+#             wicket_taken_catch (int): the number of wickets taken by the player through catches
+#             wicket_taken_bowled (int): the number of wickets taken by the player through bowled
+#             wicket_taken_stump (int): the number of wickets taken by the player through stumping
+#
+#             Returns:
+#             None
+#     """
+#
+#     # Create a connection with database
+#     ipl_db = sqlite3.connect("ipl.db")
+#     ipl_cursor = ipl_db.cursor()
+#
+#     query = "UPDATE player_stat " \
+#             "SET match_played = match_player + 1, batting_runs_scored = batting_runs_scored + ?," \
+#             "batting_bowls_played = batting_bowls_played + ?, batting_boundary = batting_boundary + ?," \
+#             "batting_six = batting_six + ?, batting_half_century = batting_half_century + ?, " \
+#             "batting_century = batting_century + ?, batting_duck_out = batting_duck_out + ?, " \
+#             "highest_score = highest_score + ?, batting_not_out = batting_not_out + ?, out_catch = out_catch + ?" \
+#             "out_run_out = out_run_out + ?, out_stumped = out_stumped + ?, out_bowled = out_bowled + ?, " \
+#             "field_catch = field_catch + ?, field_run_out = field_run_out + ?, field_stumping = field_stumping + ?," \
+#             "bowling_bowls = bowling_bowls + ?, bowling_runs = bowling_runs + ?, bowling_wickets = bowling_wickets+?," \
+#             "bowling_wicket_catch = bowling_wicket_catch + ?, bowling_wicket_bowled = bowling_wicket_bowled + ?," \
+#             "bowling_wicket_stump = bowling_wicket_stump + ?, bowling_best_figure = ? , " \
+#             "bowling_five_wickets = bowling_five_wickets + ?" \
+#             "WHERE player_name = ? LIMIT 1;"
+#
+#     # Check if player hit half-century(50 runs) or century(100 runs)
+#     century = True if batting_run >= 100 else False
+#     half_century = True if 50 <= batting_run < 100 else False
+#
+#     # Check if player is duck out/got out on 0
+#     duck_out = True if batting_run == 0 else False
+#
+#     # Check if new score of player is higher than previous highest score
+#     current_high_score = \
+#         ipl_cursor.execute('SELECT highest_score FROM player_stat WHERE name = ?', (player_name,)).fetchone()[0]
+#     new_high_score = max(batting_run, current_high_score)
+#
+#     # Type of how player got out
+#     # I am assuming that input provided by user is correct as currently I am the only one providing input
+#     out_catch = 1 if out_style == "catch" else 0
+#     out_run_out = 1 if out_style == "run-out" else 0
+#     out_stumped = 1 if out_style == "stumped" else 0
+#     out_bowled = 1 if out_style == "bowled" else 0
+#
+#     # Check if current bowling figure is best bowling figure of bowler.
+#
+#     current_best_bowling_figure = ipl_cursor.execute("SELECT bowling_best_figure FROM player_stat WHERE name = ?",
+#                                                      (player_name,)).fetchone()[0]
+#     if wicket_taken > 0:
+#         current_best_bowling_figure = ipl_cursor.execute("SELECT bowling_best_figure FROM player_stat WHERE name = ?",
+#                                                          (player_name,)).fetchone()[0]
+#         if wicket_taken >= int(current_best_bowling_figure.split("/")[1]):
+#             new_best_bowling_figure = f"{min(current_best_bowling_figure.split('/')[0], bowling_run)}/" \
+#                                       f"{current_best_bowling_figure.split('/')[1]}"
+#         else:
+#             new_best_bowling_figure = current_best_bowling_figure
+#     else:
+#         new_best_bowling_figure = current_best_bowling_figure
+#
+#     # Check if bowler got more than or equal to 5 wickets
+#     five_wickets = 1 if wicket_taken >= 5 else 0
+#
+#     ipl_cursor.execute(query, (batting_run, batting_bowl, batting_4, batting_6, int(half_century), int(century),
+#                                int(duck_out), new_high_score, not_out, out_catch, out_run_out, out_stumped,
+#                                out_bowled, catch, run_out, stumping, bowling_bowl, bowling_run, wicket_taken,
+#                                wicket_taken_catch, wicket_taken_bowled, wicket_taken_stump, new_best_bowling_figure,
+#                                five_wickets, player_name,))
+#
+#     ipl_db.commit()
+#     ipl_cursor.close()
+#     ipl_db.close()
+#
 
 def add_player_bat_data(player_name: str, match_id: int, opponent_team: str, runs: int, balls: int, boundaries: int,
-                        sixes: int, not_out: bool, out_type: str = "None", wicket_bowling_style: str = None,
+                        sixes: int, not_out: bool, out_type: str = None,
                         wicket_bowler: str = None, fielder: str = None, motm: bool = False) -> str:
     """
     Add player batting performance to two tables - Batting Performance and Player Stats Summary
@@ -114,8 +114,6 @@ def add_player_bat_data(player_name: str, match_id: int, opponent_team: str, run
     :param sixes: The total number of 6s scored by the player in this match
     :param not_out: Shows weather player was not out while batting in this match. False for out and True for not out.
     :param out_type: Shows how player got out in this match if player batted in this match.
-    :param wicket_bowling_style: Shows if player got out then what was the bowling style if player got out except by
-    run out
     :param wicket_bowler: Shows which bowler was responsible if player got out by bowled, stumped or caught. None if
     player was not out or got run out
     :param fielder: Show which fielder was responsible for player dismissal if player got caught, stumped or run out.
@@ -128,6 +126,13 @@ def add_player_bat_data(player_name: str, match_id: int, opponent_team: str, run
         # Create a connection to the database
         ipl_db = sqlite3.connect("ipl.db")
         ipl_cursor = ipl_db.cursor()
+
+        # Shows if player got out then what was the bowling style if player got out except by run out
+        if out_type in ["catch", "stumped", "bowled", "lbw"]:
+            wicket_bowling_style = ipl_cursor.execute('''SELECT "Bowling Type" FROM players WHERE Name = ? ''',
+                                                      player_name)
+        else:
+            wicket_bowling_style = None
 
         # First, add data to batting performance
         ipl_cursor.execute('''
@@ -285,8 +290,8 @@ def add_player_bowl_data(player_name: str, match_id: int, opponent_team: str, ba
 
 
 def add_match(match_id: int, home_team: str, away_team: str, stadium: str, toss_win: str, toss_win_field_first: bool,
-              winner: str, man_of_the_match: str, night_match: bool, score_inning1: int, score_inning2: int,
-              pp_score1: int, pp_score2: int, inning1_highest_score: int, inning1_highest_scorer: str,
+              winner: str, man_of_the_match: str, night_match: bool, score_inning1: str, score_inning2: str,
+              pp_score1: str, pp_score2: str, inning1_highest_score: int, inning1_highest_scorer: str,
               inning1_best_bowler: str, inning1_best_bowling: str, inning2_highest_score: int,
               inning2_highest_scorer: str, inning2_best_bowler: str, inning2_best_bowling: str) -> str:
     """
